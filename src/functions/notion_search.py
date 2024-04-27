a708ee86ac64b0edcac0c5c98daa5fa3ad7b5b69
tatsui <2360691+tatsu-i@users.noreply.github.com>
2024-04-27 16:50:35 +0900

    Update notion_search function to increase page_size and refactor page_url handling

--- src/functions/notion_search.py
+++ src/functions/notion_search.py
@@ -1,7 +1,6 @@
 import os
 import json
 import logging
-from notion.util import Notion
 from notion_client import Client
 
 
@@ -9,34 +8,26 @@ def run(keyword: str, notion_secret: str) -> str:
     """Notionのページを検索し、その内容を返します。キーワードを入力してください。"""
 
     try:
-        notion_util = Notion(notion_secret)
         notion_client = Client(auth=notion_secret)
+        page_urls = []
         # キーワードからページのURLを検索
         search_results = notion_client.search(
             query=keyword,
             sort={"direction": "descending", "timestamp": "last_edited_time"},
-            page_size=5,
+            page_size=20,
         )
         if not search_results["results"]:
             return "Notionの検索結果は何も見つかりませんでした。"
 
         # 検索結果を作成
-        res = ""
         for result in search_results["results"][:5]:
             if result["object"] != "page":
                 continue
-            try:
-                page_url = result["url"]
-                logging.info(f"ページURL: {page_url}")
-                # ページの内容を取得
-                page_content = notion_util.get_page_markdown(page_url, recursive=False)
-                # レスポンスの作成
-                res += f"# {page_url}の内容\n{page_content}\n"
-            except Exception as e:
-                logging.error(f"ページURLの取得に失敗しました: {str(e)}")
-                continue
-
-        return res
+            page_url = result["url"]
+            properties = result["properties"]
+            logging.info(f"{page_url}")
+            page_urls.append({"url": page_url, "properties": properties})
+        return json.dumps({"page_urls": page_urls})
 
     except Exception as e:
         # エラーが発生した場合
--- src/functions/open_notion_url.py
+++ src/functions/open_notion_url.py
@@ -1,8 +1,16 @@
 import logging
 from notion.util import Notion
+from timeout_decorator import timeout, TimeoutError
+
+
+@timeout(20)
+def open_url(url, notion, recursive=True):
+    return notion.get_page_markdown(url, recursive=recursive)
 
 
 def run(url, notion_secret):
     notion = Notion(notion_secret)
-    page_content = notion.get_page_markdown(url, recursive=True)
-    return page_content
+    try:
+        return open_url(url, notion, recursive=True)
+    except TimeoutError:
+        return "TimeoutError:コンテンツが大きすぎます"
